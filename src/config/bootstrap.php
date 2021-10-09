aa8499ffddbc6dba703ca408957a31a2effaa2d3
Vitaliy <theyamshikov@yandex.ru>
2021-10-09 23:08:30 +0600

    upd

--- src/config/bootstrap.php
+++ src/config/bootstrap.php
@@ -6,3 +6,12 @@ use ZnCore\Base\Legacy\Yii\Helpers\FileHelper;
 
 require_once __DIR__ . '/../Helpers/functions.php';
 requireLibs(__DIR__ . '/../Libs');
+
+App::initVarsFromArray([
+    'show_detail' => 0,
+    'sudo_cmd' => 'sudo -S {command} < ~/sudo-pass',
+    'keep_releases' => 3,
+    'allow_anonymous_stats' => 1,
+    'git_tty' => 1,
+    'default_stage' => 'staging',
+]);
